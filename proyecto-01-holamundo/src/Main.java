d7e6b9def9497d7baadb5f04713775fb68d5b375
GerardoOrgg <20253ds064@utez.edu.mx>
2026-01-12 09:00:38 -0600

    proyecto 01:hola mundo

---
+++ proyecto-01-holamundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("carranza priego naomy sayuri");
+        System.out.println("2-D");
+        System.out.println("12 de Enero del 2026");
+    }
+}
